1b0770b2d55a0bd52e2ec466ded4387b2d54053b
Ryan Alexander <ryan@onecm.com>
2015-03-26 19:25:37 -0700

    Mock GIF mode is ready for hardware functionality

--- src/gif.cpp
+++ src/gif.cpp
@@ -1,31 +1,33 @@
 #include "stak.h"
 #include "gfx.hpp"
-
-#include "entityx/entityx.h"
-#include "gtx/rotate_vector.hpp"
-#include "gtx/string_cast.hpp"
+#include "display.hpp"
+#include "timeline.hpp"
 
 // Debug
 #include <iostream>
 #include <string>
 
 using namespace otto;
+using namespace choreograph;
 
-static const int screenWidth = 96;
-static const int screenHeight = 96;
-static const vec2 screenSize = { screenWidth, screenHeight };
+static const float pi = M_PI;
+static const float twoPi = M_PI * 2.0f;
+static const float halfPi = M_PI / 2.0f;
 
-struct GifModeData {
+static Display display = { { 96.0f, 96.0f } };
+
+static struct {
   uint32_t minFrame = 1;
-  uint32_t maxFrame = 32;
+  uint32_t maxFrame = 33;
+  uint32_t nextFrame = minFrame;
 
-  float currentFrame = 55.0f;
+  float rewindAmount = 0.0f;
+
+  Output<vec3> flashColor;
 } mode;
 
 STAK_EXPORT int init() {
   loadFont(std::string(stak_assets_path()) + "232MKSD-round-medium.ttf");
-  std::cout << "gif loaded" << std::endl;
-
   return 0;
 }
 
@@ -34,139 +36,120 @@ STAK_EXPORT int shutdown() {
 }
 
 STAK_EXPORT int update(float dt) {
+  display.update([dt] { timeline.step(dt); });
   return 0;
-}
-
-static void drawFrameNumber() {
-  fontSize(28);
-  textAlign(ALIGN_CENTER | ALIGN_BASELINE);
-  fillText(std::to_string(static_cast<int>(mode.currentFrame)));
-}
-
-static void drawFrameNumberMax() {
-  fontSize(18);
-  textAlign(ALIGN_CENTER | ALIGN_TOP);
-  fillText(std::to_string(mode.maxFrame));
-}
-
-static void drawAperture(const vec3 &color, float apothem, int numBlades) {
-  ScopedTransform xf;
-  rotate(apothem * 0.015f);
-
-  std::vector<vec2> pnts;
-  pnts.emplace_back(0.0f, apothem / std::cos(float(M_PI) / numBlades));
-  for (int i = 1; i < numBlades; ++i) {
-    pnts.push_back(glm::rotate(pnts.back(), 2.0f * float(M_PI) / numBlades));
-  }
-
-  beginPath();
-  rect(screenSize * -0.5f, screenSize);
-  moveTo(pnts.front());
-  for (const auto &p : pnts) lineTo(p);
-  fillColor(color);
-  fill();
-
-  beginPath();
-  const auto *prev = &pnts.back();
-  for (const auto &p : pnts) {
-    auto v = glm::normalize(p - *prev);
-    // std::cout << glm::to_string(v) << " " << glm::to_string(p) << " " << glm::to_string(prev) << std::endl;
-    moveTo(*prev);
-    lineTo(*prev + v * 96.0f);
-    prev = &p;
-  }
-  strokeColor(glm::mix(color, vec3(0), 0.5f));
-  strokeWidth(2);
-  stroke();
 }
 
 STAK_EXPORT int draw() {
-  static const mat3 defaultMatrix = { 0.0, -1.0, 0.0, 1.0, -0.0, 0.0, 0.0, screenHeight, 1.0 };
+  display.draw([] {
+    beginPath();
+    rect(vec2(), display.bounds.size);
+    fillColor(mode.flashColor());
+    fill();
 
-  clearColor(0, 0, 0);
-  clear(0, 0, screenWidth, screenHeight);
+    translate(display.bounds.size * 0.5f);
 
-  setTransform(defaultMatrix);
-
-  // NOTE(ryan): Apply a circular mask to simulate a round display. We may want to move this to
-  // stak-sdk so that the mask is enforced.
-  fillMask(0, 0, screenWidth, screenHeight);
-  beginPath();
-  circle(48.0f, 48.0f, 48.0f);
-  beginMask();
-  fill();
-  endMask();
-  enableMask();
-
-  ScopedMask mask = { screenWidth, screenHeight };
-
-  beginPath();
-  rect(0, 0, 96, 96);
-  fillColor(1, 0, 0);
-  fill();
-
-  {
-    translate(vec2(screenSize * 0.5f));
-    {
-      ScopedTransform xf;
-      translate(0, -9.0f);
-
-      strokeColor(1, 1, 1, 0.5f);
-      fillColor(vec3(1));
+    if (mode.nextFrame <= mode.maxFrame) {
+      translate(0, -11);
 
       beginPath();
-      moveTo(-22, 0);
-      lineTo(22, 0);
-      strokeWidth(2.0f);
+      moveTo(-15, -3);
+      lineTo(0, 3);
+      lineTo(0, -3);
+      lineTo(15, 3);
+      strokeWidth(2);
+      strokeColor(vec4(1, 1, 1, 0.35f));
       stroke();
 
-      pushTransform();
-      translate(0, 10);
-      drawFrameNumber();
-      popTransform();
+      fontSize(40);
+      textAlign(ALIGN_CENTER | ALIGN_BASELINE);
+      fillColor(vec4(1));
+      fillText(std::to_string(mode.nextFrame), 0, 11);
 
-      pushTransform();
-      translate(0, -4);
-      fillColor(1, 1, 1, 0.9f);
-      drawFrameNumberMax();
-      popTransform();
+      fontSize(20);
+      textAlign(ALIGN_CENTER | ALIGN_TOP);
+      fillColor(vec4(1, 1, 1, 0.35f));
+      fillText(std::to_string(mode.maxFrame), 0, -5);
     }
+    else {
+      fontSize(50);
+      textAlign(ALIGN_CENTER | ALIGN_MIDDLE);
+      uint32_t rewindFrame = mode.maxFrame * (1.0f - mode.rewindAmount);
+      fillColor(vec3(1));
+      fillText(std::to_string(rewindFrame));
 
-    auto radius = 48.0f * (mode.currentFrame - std::floor(mode.currentFrame));
-    drawAperture(vec3(0.5f, 0, 0.75f), radius, 6);
-  }
-
+      const float width = 3.0f;
+      const float inset = 12.0f;
+      beginPath();
+      arc(vec2(), display.bounds.size - inset, halfPi + mode.rewindAmount * twoPi, halfPi);
+      strokeColor(vec4(1, 1, 1, 0.35f));
+      strokeWidth(width);
+      strokeCap(VG_CAP_BUTT);
+      stroke();
+    }
+  });
   return 0;
 }
 
-STAK_EXPORT int crank_rotated(int amount) {
-  mode.currentFrame += amount > 0 ? 0.1f : -0.1f;
-  mode.currentFrame =
-      std::max<float>(mode.minFrame, std::min<float>(mode.maxFrame, mode.currentFrame));
+static void captureFrame() {
+  static const vec3 flashColors[] = { colorBGR(0x00ADEF), colorBGR(0xEC008B), colorBGR(0xFFF100) };
+  static size_t colorIndex = 0;
 
+  if (mode.nextFrame <= mode.maxFrame) {
+    mode.nextFrame++;
+    colorIndex = (colorIndex + 1) % 3;
+    timeline.apply(&mode.flashColor)
+        .then<Hold>(flashColors[colorIndex], 0.0f)
+        .then<RampTo>(vec3(), 0.3f, EaseOutQuad());
+  }
+}
+
+static void saveFrame() {
+  mode.rewindAmount = 0.0f;
+  mode.nextFrame = mode.minFrame;
+}
+
+static void rewind(float amount) {
+  mode.rewindAmount = std::min(1.0f, mode.rewindAmount + amount);
+  if (mode.rewindAmount == 1.0f) saveFrame();
+}
+
+STAK_EXPORT int crank_rotated(int amount) {
+  if (amount > 0)
+    captureFrame();
+  else if (amount < 0 && mode.nextFrame > mode.maxFrame)
+    rewind(std::abs(amount) * 0.02f);
+  display.wake();
   return 0;
 }
 
 STAK_EXPORT int shutter_button_pressed() {
+  captureFrame();
+  display.wake();
   return 0;
 }
 
 STAK_EXPORT int shutter_button_released() {
+  display.wake();
   return 0;
 }
 
 STAK_EXPORT int power_button_pressed() {
+  display.wake();
   return 0;
 }
 
 STAK_EXPORT int power_button_released() {
+  display.wake();
   return 0;
 }
 
 STAK_EXPORT int crank_pressed() {
+  display.wake();
   return 0;
 }
 
 STAK_EXPORT int crank_released() {
+  display.wake();
   return 0;
 }
