d35a508c5a8621f009df08cc4c2cb02c0f4531a3
Ryan Alexander <ryan@onecm.com>
2015-03-31 19:47:20 -0700

    Make the film reel more phyiscal and add a better rewind indicator

--- src/gif.cpp
+++ src/gif.cpp
@@ -6,9 +6,9 @@
 
 #include "gtx/rotate_vector.hpp"
 
-// Debug
 #include <iostream>
 #include <string>
+#include <chrono>
 
 using namespace otto;
 using namespace choreograph;
@@ -28,10 +28,22 @@ static struct {
   float rewindAmountMin = 0.05f;
   float rewindAmount = 0.0f;
 
+  Output<float> frameMeterValue = nextFrame;
+  const float frameMeterRadius = 100.0f;
+  const float frameMeterAngleIncr = pi / 6.0f;
+
   Output<float> rewindMeterAmount = 0.0f;
   Output<float> rewindMeterOpacity = 0.0f;
 
   Output<vec3> flashColor;
+
+  std::chrono::steady_clock::time_point lastFrameChangeTime;
+
+  Svg *iconRewind;
+
+  void init() {
+    iconRewind = loadSvg(std::string(stak_assets_path()) + "icon-rewind.svg", "px", 96);
+  }
 
   bool isFull() { return nextFrame > maxFrame; }
   bool isEmpty() { return nextFrame == minFrame; }
@@ -41,18 +53,38 @@ static struct {
     nextFrame = minFrame;
   }
 
+  void setMeterFrame(uint32_t frame) {
+    auto now = std::chrono::steady_clock::now();
+    auto timeSinceLastFrame =
+        std::chrono::duration_cast<std::chrono::milliseconds>(now - lastFrameChangeTime).count();
+    lastFrameChangeTime = now;
+
+    // Tween to the next frame
+    float duration = timeSinceLastFrame < 200.0f ? 0.0f : 0.33f;
+    timeline.apply(&frameMeterValue).then<RampTo>(frame, duration, EaseOutQuad());
+  }
+
   void captureFrame() {
     static const vec3 flashColors[] = { colorBGR(0x00ADEF), colorBGR(0xEC008B),
                                         colorBGR(0xFFF100) };
     static size_t colorIndex = 0;
 
-    nextFrame++;
+    if (isFull()) {
+      // NOTE(ryan): Bounce the meter to indicate the reel is full
+      // if (!frameMeterValue.isConnected())
+        timeline.apply(&frameMeterValue)
+            .then<RampTo>(maxFrame + 1.15f, 0.1f, EaseOutQuad())
+            .then<RampTo>(maxFrame + 1, 0.2f, EaseInQuad());
+        return;
+    }
+
+    setMeterFrame(++nextFrame);
+
+    // Flash
     colorIndex = (colorIndex + 1) % 3;
     timeline.apply(&flashColor)
         .then<Hold>(flashColors[colorIndex], 0.0f)
         .then<RampTo>(vec3(), 0.3f, EaseOutQuad());
-
-    if (isFull()) rewind(rewindAmountMin);
   }
 
   void startRewinding() {
@@ -70,16 +102,6 @@ static struct {
   void rewind(float amount) {
     if (!isRewinding) startRewinding();
 
-    // Bounce the meter to indicate that cancelling a rewind is impossible when the reel is full.
-    if (amount < 0.0f && isFull()) {
-      if (!rewindMeterAmount.isConnected()) {
-        timeline.apply(&rewindMeterAmount)
-            .then<RampTo>(std::max(0.0f, rewindAmount - 0.05f), 0.09f, EaseOutQuad())
-            .then<RampTo>(rewindAmount, 0.09f, EaseInQuad());
-      }
-      return;
-    }
-
     rewindAmount = clamp(rewindAmount + amount, 0.0f, 1.0f);
     timeline.apply(&rewindMeterAmount).then<RampTo>(rewindAmount, 0.05f);
 
@@ -92,15 +114,119 @@ static struct {
   }
 
   void wind(float amount) {
-    if (amount < 0.0f && !isEmpty())
-      startRewinding();
-    else
+    if (amount > 0.0f) {
       captureFrame();
+    } else if (amount < 0.0f && !isEmpty()) {
+      startRewinding();
+    }
+  }
+
+  void drawFrameMeter() {
+    ScopedTransform xf;
+    translate(0, -frameMeterRadius);
+
+    fontSize(40);
+    textAlign(ALIGN_CENTER | ALIGN_BASELINE);
+
+    // NOTE(ryan): Draw one frame before and after the current frame
+    const int frame = std::round(frameMeterValue());
+    const float frameAngle = (frame - frameMeterValue()) * frameMeterAngleIncr;
+    float angle;
+    for (int i = -1; i <= 1; ++i) {
+      if (frame + i > maxFrame + 1) continue;
+
+      ScopedTransform xf;
+      angle = frameAngle + i * frameMeterAngleIncr;
+      rotate(angle);
+      translate(0, frameMeterRadius);
+
+      if (frame + i == maxFrame + 1) {
+        translate(0, 17);
+        rotate(std::min(0.0f, frameAngle) * -5.0f);
+        drawSvg(iconRewind);
+      }
+      else {
+        fillColor(1, 1, 1, 1.0f - std::abs(angle) / frameMeterAngleIncr);
+        fillText(std::to_string(frame + i));
+      }
+    }
+  }
+
+  void drawRewindMeter() {
+    float r = display.bounds.size.x * 0.5f - 8.0f;
+    float a = halfPi + rewindMeterAmount() * twoPi;
+
+    auto color = vec4(colorBGR(0xEC008B), rewindMeterOpacity());
+
+    ScopedMask mask(display.bounds.size);
+    ScopedFillRule fr(VG_NON_ZERO);
+    beginMask();
+
+    beginPath();
+    arc(vec2(), vec2(r * 2.0f), a, halfPi);
+    strokeWidth(3.0f);
+    strokeCap(VG_CAP_BUTT);
+    strokeColor(color);
+    stroke();
+
+    beginPath();
+    circle(0, r, 3);
+    circle(glm::rotate(vec2(r, 0), a), 3);
+    fillColor(color);
+    fill();
+
+    endMask();
+
+    beginPath();
+    rect(display.bounds.size * -0.5f, display.bounds.size);
+    fillColor(color);
+    fill();
+  }
+
+  void draw() {
+    beginPath();
+    rect(display.bounds.size * -0.5f, display.bounds.size);
+    fillColor(flashColor());
+    fill();
+
+    drawFrameMeter();
+
+    {
+      ScopedTransform xf;
+      translate(0, -11);
+
+      if (!isRewinding) {
+        beginPath();
+        moveTo(-15, -3);
+        lineTo(0, 3);
+        lineTo(0, -3);
+        lineTo(15, 3);
+        strokeWidth(2);
+        strokeCap(VG_CAP_ROUND);
+        strokeColor(vec4(1, 1, 1, 0.35f));
+        stroke();
+      }
+
+      fillColor(vec4(1, 1, 1, 0.35f));
+      if (isRewinding) {
+        fontSize(16);
+        textAlign(ALIGN_CENTER | ALIGN_MIDDLE);
+        fillText("rewind", 0, -4);
+      } else {
+        fontSize(20);
+        textAlign(ALIGN_CENTER | ALIGN_TOP);
+        fillText(std::to_string(maxFrame), 0, -5);
+      }
+    }
+
+    // Rewinding
+    if (rewindMeterOpacity > 0.0f) drawRewindMeter();
   }
 } mode;
 
 STAK_EXPORT int init() {
   loadFont(std::string(stak_assets_path()) + "232MKSD-round-medium.ttf");
+  mode.init();
   return 0;
 }
 
@@ -115,83 +241,8 @@ STAK_EXPORT int update(float dt) {
 
 STAK_EXPORT int draw() {
   display.draw([] {
-    beginPath();
-    rect(vec2(), display.bounds.size);
-    fillColor(mode.flashColor());
-    fill();
-
     translate(display.bounds.size * 0.5f);
-
-    {
-      ScopedTransform xf;
-      translate(0, -11);
-
-      uint32_t frameNum = mode.nextFrame;
-      if (mode.isRewinding) {
-        frameNum = map(mode.rewindAmount, mode.rewindAmountMin, 1.0f,
-                       std::min(mode.maxFrame, mode.nextFrame), mode.minFrame);
-      }
-
-      fontSize(40);
-      textAlign(ALIGN_CENTER | ALIGN_BASELINE);
-      fillColor(vec4(1));
-      fillText(std::to_string(frameNum), 0, 11);
-
-      if (!mode.isRewinding) {
-        beginPath();
-        moveTo(-15, -3);
-        lineTo(0, 3);
-        lineTo(0, -3);
-        lineTo(15, 3);
-        strokeWidth(2);
-        strokeCap(VG_CAP_ROUND);
-        strokeColor(vec4(1, 1, 1, 0.35f));
-        stroke();
-      }
-
-      fillColor(vec4(1, 1, 1, 0.35f));
-      if (mode.isRewinding) {
-        fontSize(16);
-        textAlign(ALIGN_CENTER | ALIGN_MIDDLE);
-        fillText("rewind", 0, -4);
-      } else {
-        fontSize(20);
-        textAlign(ALIGN_CENTER | ALIGN_TOP);
-        fillText(std::to_string(mode.maxFrame), 0, -5);
-      }
-    }
-
-    // Rewinding
-    if (mode.rewindMeterOpacity > 0.0f) {
-      float r = display.bounds.size.x * 0.5f - 8.0f;
-      float a = halfPi + mode.rewindMeterAmount() * twoPi;
-
-      auto color = vec4(colorBGR(0xEC008B), mode.rewindMeterOpacity());
-
-      ScopedMask mask(display.bounds.size);
-      ScopedFillRule fr(VG_NON_ZERO);
-      beginMask();
-
-      beginPath();
-      arc(vec2(), vec2(r * 2.0f), a, halfPi);
-      strokeWidth(3.0f);
-      strokeCap(VG_CAP_BUTT);
-      strokeColor(color);
-      stroke();
-
-      beginPath();
-      circle(0, r, 3);
-      circle(glm::rotate(vec2(r, 0), a), 3);
-      fillColor(color);
-      fill();
-
-      endMask();
-
-      beginPath();
-      rect(display.bounds.size * -0.5f, display.bounds.size);
-      fillColor(color);
-      fill();
-    }
+    mode.draw();
   });
   return 0;
 }
